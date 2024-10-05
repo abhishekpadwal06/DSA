d60ba95aefbe423f4fa8bc51102bc0afc5f9cba0
Abhishek Padwal <abhishekspadwal0610@gmail.com>
2024-10-05 23:09:04 +0530

    Array Problems

---
+++ Array Problems/Second-Largest-Element-in-the-Array.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() { 
+    int n;
+    cout<<"Enter array size: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter array elements: ";
+    for(int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+}
