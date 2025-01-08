27119340ad60546e4787c28a65bb1b838bdded67
Abhishek Padwal <abhishekspadwal0610@gmail.com>
2025-01-08 23:09:35 +0530

    Factorial Program

---
+++ Basic Math Problems/Factorial.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int fact = 1;
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    if(num <= 1) {
+        cout<<"Factorial of "<<num<<" is "<<num;
+    }
+    for(int i=1; i<=num; i++) {
+        fact *= i;
+    }
+    cout<<"Factorial: "<<fact;
+    return 0;
+}
