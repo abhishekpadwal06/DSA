a318f1c77b35552fc3e97d9cc74f1e4355f66177
abhishekpadwal06 <abhishekspadwal0610@gmail.com>
2025-08-13 11:09:59 +0530

    Minor changes in 2nd Optimal code

--- Array Problems/Merge_2_Sorted_Arrays_OPTIMAL_2.cpp
+++ Array Problems/Merge_2_Sorted_Arrays_OPTIMAL_2.cpp
@@ -1,49 +1,57 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class solution{
+
+class Solution {
 private:
     void swapIfGreater(vector<int>& nums1, vector<int>& nums2, int ind1, int ind2) {
-        if(nums1[ind1] > nums2[ind2]) {
+        if (nums1[ind1] > nums2[ind2]) {
             swap(nums1[ind1], nums2[ind2]);
         }
     }
+
 public:
-    void merge(vector<int>& nums1, int m,vector<int>& nums2, int n) {
-        int len = (m + n);
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int len = m + n;
         int gap = (len / 2) + (len % 2);
-        while(gap > 0) {
+
+        while (gap > 0) {
             int left = 0;
             int right = left + gap;
-            while(right < len) {
-                // nums1 and nums2 
-                if(left < m && right >= n) {
+
+            while (right < len) {
+                // nums1 and nums1
+                if (left < m && right < m) {
+                    swapIfGreater(nums1, nums1, left, right);
+                }
+                // nums1 and nums2
+                else if (left < m && right >= m) {
                     swapIfGreater(nums1, nums2, left, right - m);
                 }
                 // nums2 and nums2
-                else if(left >= m) {
+                else {
                     swapIfGreater(nums2, nums2, left - m, right - m);
                 }
-                // nums1 and nums1
-                else {
-                    swapIfGreater(nums1, nums1, left, right);
-                }
+
+                left++;
+                right++;
             }
-            left++, right++;
-            if(gap == 1)    break;
+
+            if (gap == 1) break;
             gap = (gap / 2) + (gap % 2);
         }
 
-        cout<<"Sorted arrays: \nnums1: ";
-        for(int i=0; i<m; i++) {
+        cout << "Sorted arrays:\nnums1: ";
+        for (int i = 0; i < m; i++) {
             cout << nums1[i] << " ";
         }
-        cout<<"\nnums2: ";
-        for(int i=0; i<n; i++) {
+        cout << "\nnums2: ";
+        for (int i = 0; i < n; i++) {
             cout << nums2[i] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 };
+
 int main() {
     int m, n;
     cout << "Enter array 1 size: ";
@@ -63,7 +71,7 @@ int main() {
         cin >> nums2[i];
     }
 
-    solution s;
+    Solution s;
     s.merge(nums1, m, nums2, n);
 
     return 0;
